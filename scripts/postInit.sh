58047357d37f6a3452cedd4c106bcfcaf0ed921f
RYPTAR <ray.dej21@gmail.com>
2023-12-15 11:06:19 -0700

    Implementing package config rebuild

--- scripts/postInit.sh
+++ scripts/postInit.sh
@@ -2,8 +2,8 @@
 
 echo "\nRebuilding react-native.config file...\n"
 # "\nEditing iOS flags...\n"
-#rm react-native.config.js
-#echo "// CONFIG: React Native Vector Icons iOS\nmodule.exports = {\n\tdependencies: {\n\t\t\'react-native-vector-icons\': {\n\t\t\tplatforms: {\n\t\t\t\tios: null,\n\t\t\t},\n\t\t},\n\t},\n};\n" > react-native.config.js
+rm react-native.config.js
+echo "// CONFIG: React Native Vector Icons iOS\nmodule.exports = {\n\tdependencies: {\n\t\t\'react-native-vector-icons\': {\n\t\t\tplatforms: {\n\t\t\t\tios: null,\n\t\t\t},\n\t\t},\n\t},\n};\n" > react-native.config.js
 
 # Package config
 #   // CONFIG: React Native Vector Icons iOS
