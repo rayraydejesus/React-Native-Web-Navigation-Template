624d28cd66b14ef85e3ce30f9c4d6da7b09d59db
RYPTAR <ray.dej21@gmail.com>
2023-12-14 17:27:35 -0700

    Moved post init script

--- template.config.js
+++ template.config.js
@@ -7,5 +7,5 @@ module.exports = {
   templateDir: './template',
 
   // Path to script, which will be executed after init
-  postInitScript: './scripts/post-init.script.sh',
+  postInitScript: './post-init.script.sh',
 };
